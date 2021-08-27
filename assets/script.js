05a964381bb703f3276ebba5f8c257127f565836
AustinxEwell <austin.ewell86@gmail.com>
2021-08-27 10:23:17 -0600

    updated css to work properly.. again fixed current day

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,35 @@
 
-    var clockElement = document.getElementById('currentDay');
+    // var clockElement = document.getElementById('currentDay');
 
-    function clock() {
-        clockElement.textContent = moment().format('dddd MMMM Do')
-    }
+    // function clock() {
+    //     clockElement.textContent = moment().format('dddd MMMM Do')
+    // }
+    
+    var currentDay = moment().format('dddd,  MMMM Do');
+    $('#currentDay').append(currentDay);
+
+
+
+
+    $(".saveBtn").on("click", function () {
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, value);
+    });
+
+    // $(".saveBtn").on("click", "p", function() {
+    //     var text = $(this).text().trim();
+
+    //     var textInput = $("<textarea>")
+    //     .addClass("textarea")
+    //     .val(text)
+
+
+    //     $(this).replaceWith(textInput);
+    //     textInput.trigger("focus");
+    //   });
+
 
     setInterval(clock, 1000);
+
+
